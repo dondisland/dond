8fe147bcc845622969d06c3643819fd4465de39c
halilbayir <bayirhalil3@gmail.com>
2016-03-08 21:16:15 +0200

    initial commit

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class UserController : ApiController
+    {
+        List<User> users = new List<User>() { new User { email = "bayirhalil@gmail.com", nickName = "HB", password = "2wes4443" } };
+
+        public IHttpActionResult Login(User user)
+        {
+            var ifUser = users.FirstOrDefault(x => x.email.Equals(user.email) && x.nickName.Equals(user.nickName) && x.password.Equals(user.password));
+            if (ifUser == null)
+            {
+                return Ok("bb");
+            }
+            return Ok("Giris Tamam");
+        }
+
+
+    }
+}
---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            App_Start.RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
